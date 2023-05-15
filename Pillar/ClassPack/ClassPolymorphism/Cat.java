c889dcd5e40fd6edde9a8ebae10f019b4c3a7fe6
Miko Clark Cruz <110460613+Mikouxei@users.noreply.github.com>
2023-05-15 11:39:18 +0800

    Add files via upload

---
+++ Pillar/ClassPack/ClassPolymorphism/Cat.java
@@ -0,0 +1,8 @@
+package ClassPack.ClassPolymorphism;
+
+public class Cat extends Animal{
+    //overriding
+    public void eat(){
+        System.out.print("skrt skrt");
+    }
+}
---
+++ Pillar/ClassPack/ClassPolymorphism/Dog.java
@@ -0,0 +1,11 @@
+package ClassPack.ClassPolymorphism;
+
+public class Dog extends Animal{
+    //overloading
+    public void eat(int number){
+        for(int i=0; i<=number; i++){
+            System.out.print("aw aw");
+        }
+        
+    }
+}

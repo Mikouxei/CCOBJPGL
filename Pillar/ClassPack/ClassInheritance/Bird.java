fbd0eb4b81fb25978dbd19fc39b83ff076aac5df
Miko Clark Cruz <110460613+Mikouxei@users.noreply.github.com>
2023-05-15 11:37:33 +0800

    Add files via upload

---
+++ Pillar/ClassPack/ClassInheritance/Bird.java
@@ -0,0 +1,5 @@
+package ClassPack.ClassInheritance;
+
+public class Bird extends Animal{
+    public int wings = 2;
+}
---
+++ Pillar/ClassPack/ClassInheritance/Cat.java
@@ -0,0 +1,5 @@
+package ClassPack.ClassInheritance;
+
+public class Cat extends Animal{
+    public int legs = 4;
+}
